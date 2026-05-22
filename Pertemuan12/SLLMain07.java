0d184845693ab2ce2262a7ba6cd63233cd4637e1
ChristianNugraha06 <christiannugraha5@gmail.com>
2026-05-22 10:39:09 +0700

    Edit pertanyaan no 3 percobaan 1

--- Pertemuan12/SLLMain07.java
+++ Pertemuan12/SLLMain07.java
@@ -1,31 +1,69 @@
 package Pertemuan12;
 
+import java.util.Scanner;
+
 public class SLLMain07 {
+      static Scanner sc = new Scanner (System.in);
+
+       static mahasiswa07 inputMahasiswa() {
+        System.out.print("  Masukkan NIM   : ");
+        String nim = sc.nextLine();
+        System.out.print("  Masukkan Nama  : ");
+        String nama = sc.nextLine();
+        System.out.print("  Masukkan Kelas : ");
+        String kelas = sc.nextLine();
+        System.out.print("  Masukkan IPK   : ");
+        double ipk = sc.nextDouble();
+        sc.nextLine(); 
+        return new mahasiswa07(nim, nama, kelas, ipk);
+    }
+
     public static void main(String[] args) {
         SingleLinkedList07 sll = new SingleLinkedList07();
 
-        mahasiswa07 mhs1 = new mahasiswa07("24212200", "Alvaro", "1A", 4.0);
-        mahasiswa07 mhs2 = new mahasiswa07("22212202", "Cintia", "3C", 3.5);
-        mahasiswa07 mhs3 = new mahasiswa07("23212201", "Bimon",  "2B", 3.8);
-        mahasiswa07 mhs4 = new mahasiswa07("21212203", "Dirga",  "4D", 3.6);
+           System.out.println("========================================");
+        System.out.println("  INPUT DATA MAHASISWA DARI KEYBOARD");
+        System.out.println("========================================");
  
-       
-        sll.print();
- 
-       
-        sll.addFirst(mhs4);
+        
         sll.print();
  
         
-        sll.addLast(mhs1);
+        System.out.println(" Masukkan data mahasiswa ke-1:");
+        mahasiswa07 mhs1 = inputMahasiswa();
+        sll.addFirst(mhs1);
+        sll.print();
+ 
+        
+        System.out.println(" Masukkan data mahasiswa ke-2:");
+        mahasiswa07 mhs2 = inputMahasiswa();
+        sll.addLast(mhs2);
+        sll.print();
+ 
+        
+        System.out.print(" Masukkan nama mahasiswa yang dijadikan acuan: ");
+        String key = sc.nextLine();
+        System.out.println("[insertAfter] Masukkan data mahasiswa baru:");
+        mahasiswa07 mhs3 = inputMahasiswa();
+        sll.insertAfter(key, mhs3);
         sll.print();
  
        
-        sll.insertAfter("Dirga", mhs3);
- 
-      
-        sll.insertAt(2, mhs2);
+        System.out.print(" Masukkan indeks tempat mahasiswa akan disisipkan: ");
+        int index = sc.nextInt();
+        sc.nextLine(); 
+        System.out.println(" Masukkan data mahasiswa baru:");
+        mahasiswa07 mhs4 = inputMahasiswa();
+        sll.insertAt(index, mhs4);
         sll.print();
+ 
+       
     }
-    }
+
+} 
+
+
+        
+    
+
 
