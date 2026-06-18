bcd6fc05a3b21a6e7692ba32e519567ee1004219
ChristianNugraha06 <christiannugraha5@gmail.com>
2026-06-18 20:30:23 +0700

    Percobaan 1 jobsheet 14

---
+++ Pertemuan14/Node07.java
@@ -0,0 +1,15 @@
+package Pertemuan14;
+
+public class Node07 {
+    mahasiswa07 mahasiswa;
+    Node07 left, right;
+
+    public Node07(){
+
+    }
+
+    public Node07 (mahasiswa07 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
