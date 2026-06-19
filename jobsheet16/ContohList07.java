15a6268c4fccbb558e09f5128bdbb2d33a06a034
ChristianNugraha06 <christiannugraha5@gmail.com>
2026-06-19 16:03:45 +0700

    Percobaan 1 jobsheet 16

--- jobsheet16/ContohList07.java
+++ jobsheet16/ContohList07.java
@@ -19,12 +19,6 @@ public class ContohList07 {
         System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n", l.get(0), l.size(), l.get(l.size()-1));
 
 
-
-
-
-
-
-
         List<String> names = new LinkedList<>();
         names.add("Noureen");
         names.add("Akhleema");
@@ -37,9 +31,6 @@ public class ContohList07 {
         System.out.printf("Elemen 0: %s total elemen: %s elemen terakhir: %s\n", names.get(0), names.size(), names.get(names.size()-1));
 
         System.out.println("Names: " + names.toString());
-
-
-
 
 
     }
